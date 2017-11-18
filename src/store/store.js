1a76cea0cc2111afcc12bd129da0c6beb99593e8
陈小龙 <13480731740@qq.com>
2017-11-18 15:33:16 +0800

    vuex getters 方法 简便方法组件中直接调用即可

--- src/store/store.js
+++ src/store/store.js
@@ -9,5 +9,16 @@ export const store = new Vuex.Store({
             { name: '陈小龙', age: 29, price: '300' },
             { name: '陈占译', age: 3, price: '600' }
         ]
+    },
+    getters: {
+        saleUse(state) {
+            var saleUser = state.users.map(user => {
+                return {
+                    name: "**" + user.name + "**",
+                    price: user.price / 2
+                };
+            });
+            return saleUser;
+        }
     }
 });
